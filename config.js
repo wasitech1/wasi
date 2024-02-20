e0fe8d288b7bb375b53d0cf5e2a2b77a87bd6470
WASI <142110420+Itxxwasi@users.noreply.github.com>
2024-02-20 19:49:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ module.exports = {
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API  || "false",
 
-  REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
+  REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "7W1tWMq2JptmPPzFPJvhngNb",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
